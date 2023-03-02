e8b0a989e9707ec4118e76b11a4d44fbf669cc6a
tanvirhasn <tanvirhasan.edu.bd@gmail.com>
2023-03-02 23:54:09 +0600

    loadAiData function added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const loadAiData = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data.tools);
+}
+
+loadAiData();
